2bf0acfa6ebb158ce762e34d5202c355b23053f2
Ankish <ankish5@gmail.com>
2014-09-15 20:30:58 +0530

    init

---
+++ samplebraintree/samplebraintree/VMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VMAppDelegate.h
+//  samplebraintree
+//
+//  Created by Ankish Jain on 15/09/14.
+//  Copyright (c) 2014 app. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ samplebraintree/samplebraintree/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  samplebraintree
+//
+//  Created by Ankish Jain on 15/09/14.
+//  Copyright (c) 2014 app. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VMAppDelegate class]));
+    }
+}
